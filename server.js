ef079ab20045d14e9b8952fbb8814f3ae335cc93
Ana Bahena <ana.gonzalezb@ab-inbev.com>
2023-08-09 21:09:27 -0600

    feat: modify vercel

--- server.js
+++ server.js
@@ -53,15 +53,12 @@ const { GraphQLServer } = require("graphql-yoga"),
 
 async function startApolloServer() {
   const app = express();
-  // Our httpServer handles incoming requests to our Express app.
-  // Below, we tell Apollo Server to "drain" this httpServer,
-  // enabling our servers to shut down gracefully.
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     csrfPrevention: true,
-    cache: 'bounded',
+    cache: "bounded",
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
@@ -69,9 +66,13 @@ async function startApolloServer() {
 
   // Mount Apollo middleware here.
   server.applyMiddleware({ app });
-  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 4000 }, resolve)).then(()=> console.log('good')).catch((err)=> console.log('err', err));
+  await new Promise((resolve) =>
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+  )
+    .then(() => console.log("good"))
+    .catch((err) => console.log("err", err));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
   return { server, app };
 }
 
-startApolloServer()
+startApolloServer();
