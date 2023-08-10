cb1938d1b3273d2e75b4ef1662775779e90c74d2
Ana Bahena <ana.gonzalezb@ab-inbev.com>
2023-08-09 21:01:36 -0600

    feat: modify vercel

--- server.js
+++ server.js
@@ -1,46 +1,77 @@
 const { GraphQLServer } = require("graphql-yoga"),
   express = require("express"),
   cookieParser = require("cookie-parser"),
-  useragent = require("express-useragent"),
+  http = require("http"),
   typeDefs = require("./typeDefs"),
+  { ApolloServer } = require("apollo-server-express"),
+  { ApolloServerPluginDrainHttpServer } = require("apollo-server-core"),
+  cors = require("cors"),
   resolvers = require("./resolvers");
 
-const sockets = new Map();
-(options = {
-  uploads: {
-    maxFieldSize: 1000000000,
-    maxFileSize: 1000000000,
-  },
-  bodyParserOptions: {
-    limit: "10mb",
-    extended: true,
-  },
-  port: process.env.PORT || 4000,
-}),
-  (server = new GraphQLServer({
+// const options = {
+//   uploads: {
+//     maxFieldSize: 1000000000,
+//     maxFileSize: 1000000000,
+//   },
+//   bodyParserOptions: {
+//     limit: "10mb",
+//     extended: true,
+//   },
+//   port: process.env.PORT || 4000,
+// };
+// const server = new GraphQLServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const app = express();
+// const httpServer = http.createServer(app);
+
+// const startApolloServer = async (app, httpServer) => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+// };
+
+// server.express.use(async (req, res, next) => {
+//   next()
+// });
+
+// server
+//   .start(options)
+//   .then(() => {
+//     console.log(`Server is running 🚀🚀 on localhost:${options.port}`);
+//   })
+//   .catch((error) => {
+//     console.error("GraphQL Error", error);
+//   });
+
+async function startApolloServer() {
+  const app = express();
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
     typeDefs,
     resolvers,
-  }));
-
-server.express.set("trust proxy", true);
-
-server.express.use(useragent.express());
-
-server.express.use(cookieParser());
-
-server.express.use(express.json());
-
-server.express.use(async (req, res, next) => {
-  next();
-});
-
-server
-  .start(options)
-  .then(() => {
-    console.log(`Server is running 🚀🚀 on localhost:${options.port}`);
-  })
-  .catch((error) => {
-    console.error("GraphQL Error", error);
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-module.exports = server;
+  await server.start();
+
+  // Mount Apollo middleware here.
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
+
+startApolloServer()
