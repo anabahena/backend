82fba2551128ce991b9f744010e5f94aaa138e3f
Ana Bahena <ana.gonzalezb@ab-inbev.com>
2023-08-09 21:04:26 -0600

    feat: modify vercel

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ async function startApolloServer() {
 
   // Mount Apollo middleware here.
   server.applyMiddleware({ app });
-  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
   return { server, app };
 }
